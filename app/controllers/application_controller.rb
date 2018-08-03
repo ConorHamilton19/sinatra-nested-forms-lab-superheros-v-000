0431dffffeb97e865096e4e21d9039106fa1dc39
Conor Hamilton <conhamil19@gmail.com>
2018-08-03 20:35:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,16 @@ class App < Sinatra::Base
   end
   
   post '/teams' do
-    binding.pry 
     
-    erb :'team'
+    @team = Team.new(params[:team])
+
+		params[:team][:members].each do |details|
+			SuperHero.new(details)
+		end
+
+		@super_heroes = SuperHero.all
+
+		erb :'team'
   end
 
 
